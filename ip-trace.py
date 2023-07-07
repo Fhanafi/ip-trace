cc5c915faea27f877c764ae55d6c00990b1f16c1
Han <89624707+Fhanafi@users.noreply.github.com>
2023-07-07 08:27:49 +0700

    update ip-trace.py

--- ip-trace.py
+++ ip-trace.py
@@ -1,20 +1,9 @@
 import requests
-import folium
+import pprint
 
 ip = input("Masukkan IP: ")
 url = f"https://ipapi.co/{ip}/json"
 
 r = requests.get(url)
-data = r.json()
+pprint.pprint(r.json())
 
-latitude = float(data['latitude'])
-longitude = float(data['longitude'])
-
-# Membuat objek peta menggunakan folium
-m = folium.Map(location=[latitude, longitude], zoom_start=10)
-
-# Menandai lokasi pada peta
-folium.Marker([latitude, longitude], popup=f"IP: {ip}").add_to(m)
-
-# Menampilkan peta
-m.save('map.html')
